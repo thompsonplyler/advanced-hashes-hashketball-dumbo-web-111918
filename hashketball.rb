4246d95b1b9346a2b7595d29ae98e7aa56dde94f
Thompson Plyler <nealok.g@gmail.com>
2018-11-12 05:46:39 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,7 +126,7 @@ def num_points_scored(player_name)
       if key1 == :players
         value1.collect do |player, stats|
           if player == player_name
-            player_hash << player
+            player_hash = player
             stats.collect do |stat,value|
               if stat = :points
                 value
